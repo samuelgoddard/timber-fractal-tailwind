59dcb7fbd23d8eb7bfbd862f4ed22b5521a41f58
samuelgoddard <hello@samgoddard.co.uk>
2019-06-25 11:41:29 +0100

    removed custom login page php

--- app/config/wp/login-page.php
+++ app/config/wp/login-page.php
@@ -1,18 +1,6 @@
 <?php
 
 /**
- * Login Page logo title
- *
- * @return string
- */
-function skeleton_login_logo_title()
-{
-    return 'Skeleton';
-}
-
-add_filter('login_headertitle', 'skeleton_login_logo_title');
-
-/**
  * Add favicon to Login Page
  */
 add_action('login_head', function () {
