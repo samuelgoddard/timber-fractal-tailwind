29b3c1da4322c499250787ac7046ba2f00e23cde
samuelgoddard <hello@samgoddard.co.uk>
2019-06-25 11:43:23 +0100

    mobile menu triggered as a module

--- resources/assets/js/modulesEnabled.js
+++ resources/assets/js/modulesEnabled.js
@@ -1,10 +1,12 @@
 // import { breakpointDebug } from "@/components/debug/breakpoint/index";
 
+import { mobileMenu } from "../../components/molecules/mobile-menu";
 import { bindToggles } from "./components/toggle/index";
 // import { bindMidiAccordions } from "./components/toggle/index";
 
 bindToggles();
 
 export default {
-  // breakpointDebug,
+	// breakpointDebug,
+	mobileMenu,
 };
